5df5f96d004caf1e13661a9d962865a67015472f
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-12-10 19:59:16 +0100

    Add another custom command - importdata - that can import data from CSV file to DB and is model agnostic.

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Customer
 
 
 admin.site.register(Student)
+admin.site.register(Customer)
--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,5 +1,5 @@
-from django.core.management.base import BaseCommand
-from dataentry.models import Student
+from django.core.management.base import BaseCommand, CommandError
+from django.apps import apps
 import csv
 
 class Command(BaseCommand):
@@ -8,19 +8,45 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('filepath', type=str, help="Path to a CSV file.")
+        parser.add_argument('model_name', type=str, help="Name of the model.")
 
     def handle(self, *args, **kwaregs):
         # Custom command logic.
         csv_filepath = kwaregs["filepath"]
-        
+        model_name = kwaregs["model_name"].capitalize()
+        model = None
+
+        # Search for model across all installed apps.
+        for app_config in apps.get_app_configs():
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break # model found 
+            except LookupError:
+                continue # model not found, continue searching.
+
+        if not model:
+            raise CommandError(f"Model '{model_name}' not found in any app!")
+
         with open(csv_filepath, "r") as file:
             reader = csv.DictReader(file)
+
+            if not reader.fieldnames:
+                raise CommandError("CSV file is empty or missing headers!")
+            
+            unique_field = reader.fieldnames[0]
+            field_names = [field.name for field in model._meta.get_fields()]
+
+            if unique_field not in field_names:
+                raise CommandError(f"Field '{unique_field}' does not exist in model '{model_name}'!")
+
             for data in reader:
-                roll_no = data["roll_no"]
-                record_exists = Student.objects.filter(roll_no=roll_no).exists()
+                unique_value = data[unique_field]
+                record_exists = model.objects.filter(**{unique_field: unique_value}).exists()
+
                 if not record_exists:
-                    Student.objects.create(**data)
+                    model.objects.create(**data)
+                    self.stdout.write(self.style.SUCCESS(f"Added new record with {unique_field}={unique_value}"))
                 else: 
-                    self.stdout.write(self.style.WARNING(f"Student with roll_no {roll_no} already exists in DB!"))
+                    self.stdout.write(self.style.WARNING(f"Record with {unique_field}={unique_value} already exists!"))
 
         self.stdout.write(self.style.SUCCESS("Data inserted from CSV file succesfully!"))
