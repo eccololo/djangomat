07ce17529f8d9b111c7d6f15e5793f33831a3274
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-12-11 18:12:52 +0100

    Adding styles to insert-data form page.

--- dataentry/views.py
+++ dataentry/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def import_data(request):
+    
+    if request.method == "POST":
+        pass
+    else:
+        all_models = pass
+
     return render(request, "dataentry/importdata.html", {})
