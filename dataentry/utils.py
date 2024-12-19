fbf590c2a393552c125dd70115a0637437ca16f4
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-12-19 21:54:49 +0100

    Add import data from a CSV file to a model functionality from a front end page and form.

--- dataentry/utils.py
+++ dataentry/utils.py
@@ -5,7 +5,7 @@ def get_all_custom_models():
     all_models = set()
     # Default models from Django installation. If a new model will be added it should be added to this list.
     # YOu can add "User" model to this list to prevent from importing user data to DB.
-    default_models = {"LogEntry", "Permission", "Group", "ContentType", "Session"}
+    default_models = {"LogEntry", "Permission", "Group", "ContentType", "Session", "Upload"}
 
     for model in apps.get_models():
         all_models.add(model.__name__)
--- dataentry/views.py
+++ dataentry/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .utils import get_all_custom_models
+from uploads.models import Upload
+from django.conf import settings
+from django.core.management import call_command
 
 def import_data(request):
 
     if request.method == "POST":
-        pass
+        file_path = request.FILES.get("file_path")
+        model_name = request.POST.get("model_name")
+
+        upload = Upload.objects.create(model_name=model_name, file=file_path)
+
+        relative_path = str(upload.file.url)
+        base_url = str(settings.BASE_DIR)
+
+        file_path = base_url + relative_path
+
+        corrected_path = file_path.replace("/", "\\")
+        try:
+            call_command("importdata", corrected_path, model_name)
+        except Exception as e:
+            raise e
+
+        return redirect("import_data")
     else:
         custom_models = get_all_custom_models()
         context = {
