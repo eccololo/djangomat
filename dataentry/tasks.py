7f538c9cde64cfeb887b09547f2572c7bb511df4
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-12-25 12:35:08 +0100

    Add import data from CSV file to DB as a Celery task.

--- dataentry/tasks.py
+++ dataentry/tasks.py
@@ -1,7 +1,17 @@
 from djangomat.celery import app as celery_app
+from django.core.management import call_command
 import time
 
 @celery_app.task
 def celery_test_task():
     time.sleep(10)
     return "Task executed succesfully!"
+
+@celery_app.task
+def import_data_task(corrected_path, model_name):
+    try:
+        call_command("importdata", corrected_path, model_name)
+    except Exception as e:
+        raise e
+    
+    return "Data imported succesfully!"
--- dataentry/views.py
+++ dataentry/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
-from .utils import get_all_custom_models
 from uploads.models import Upload
 from django.conf import settings
-from django.core.management import call_command
 from django.contrib import messages
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
+from .tasks import import_data_task
+from .utils import get_all_custom_models
 
+
+# @method_decorator(csrf_protect, name='dispatch')
+# @method_decorator(login_required, name='dispatch')
+@csrf_protect
+@login_required
 def import_data(request):
 
     if request.method == "POST":
@@ -19,12 +27,10 @@ def import_data(request):
         file_path = base_url + relative_path
 
         corrected_path = file_path.replace("/", "\\")
-        try:
-            call_command("importdata", corrected_path, model_name)
-            messages.success(request, "Data imported successfully!")
-        except Exception as e:
-            messages.error(request, str(e))
 
+        # Celery task here ...
+        import_data_task.delay(corrected_path, model_name)
+        messages.success(request, "Your data is being imported. You will be notified once it is done!")
 
         return redirect("import_data")
     else:
@@ -33,4 +39,4 @@ def import_data(request):
             "custom_models": custom_models
         }
 
-    return render(request, "dataentry/importdata.html", context)
+        return render(request, "dataentry/importdata.html", context)
