145c68ce79d850cbaf98a38d45bdba97b4866f0a
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-01-01 16:15:05 +0100

    Add simple registration form to for user.

--- djangomat/urls.py
+++ djangomat/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("dataentry/", include("dataentry.urls")),
     path("celery-test/", views.celery_test),
+    path("register", views.register, name="register"),
+    path("login", views.login, name="login"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djangomat/views.py
+++ djangomat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from dataentry.tasks import celery_test_task
+from .forms import RegistrationForm
 
 def home(request):
     return render(request, "home.html", {})
@@ -9,3 +10,18 @@ def home(request):
 def celery_test(request):
     msg = celery_test_task.delay()
     return HttpResponse("<h3>Celery Test Works Well!</h3>")
+
+def register(request):
+
+    if request.method == "POST":
+        pass
+    else:
+        form = RegistrationForm()
+        context = {
+            'form': form
+        }
+
+    return render(request, "register.html", context)
+
+def login(request):
+    return render(request, "login.html")
