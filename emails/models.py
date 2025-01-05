fdba1b92d4cf6716e19694e1f3f7c90a6730f898
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-01-05 18:30:59 +0100

    Add models to Email app.

--- emails/models.py
+++ emails/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+
+    email_list = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.email_list
+    
+
+class Subscriber(models.Model):
+
+    email_address = models.EmailField(max_length=50)
+    # When list will be deleted as well deleted will be subscriber.
+    email_list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.email_address
+
+class Email(models.Model):
+
+    subject = models.CharField(max_length=100)
+    body = models.TextField(max_length=700)
+    attachment = models.FileField(upload_to="email_attachments/")
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    email_list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
