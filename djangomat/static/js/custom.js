4a1995f8e47ccc14c8f3d263474cd9f0e6ca06b2
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-01-09 09:15:50 +0100

    Add search feature that user can search for automation task on home page by writing task title in search bar.

--- djangomat/static/js/custom.js
+++ djangomat/static/js/custom.js
@@ -1,3 +1,36 @@
+// Scrolling up alert message that it will dissapear after 5 seconds
 setTimeout(function() {
     $("#messages").slideUp("slow");
 }, 5000);
+
+// Code for search bar on home page to search for automation tasks.
+document.addEventListener('DOMContentLoaded', function () {
+    const searchBar = document.getElementById('searchBar');
+    const taskContainer = document.getElementById('taskContainer');
+    const cards = taskContainer.getElementsByClassName('col-md-4');
+
+    searchBar.addEventListener('input', function () {
+        const filter = searchBar.value.toLowerCase();
+
+        Array.from(cards).forEach(card => {
+            const title = card.querySelector('.card-title').innerText.toLowerCase();
+            if (title.includes(filter)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+            
+            // Deleting top margin after search to align card good
+            card.classList.remove('mt-5');
+        });
+
+        if (filter === '') {
+            Array.from(cards).forEach((card, index) => {
+                if (index >= 3) {
+                    // Adding top margin to tasks cards but not to first 3 when search field is empty.
+                    card.classList.add('mt-5');
+                }
+            });
+        }
+    });
+});
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,24 @@
+// Scrolling up alert message that it will dissapear after 5 seconds
 setTimeout(function() {
     $("#messages").slideUp("slow");
 }, 5000);
+
+// Code for search bar on home page to search for automation tasks.
+document.addEventListener('DOMContentLoaded', function () {
+    const searchBar = document.getElementById('searchBar');
+    const taskContainer = document.getElementById('taskContainer');
+    const cards = taskContainer.getElementsByClassName('col-md-4');
+
+    searchBar.addEventListener('input', function () {
+        const filter = searchBar.value.toLowerCase();
+
+        Array.from(cards).forEach(card => {
+            const title = card.querySelector('.card-title').innerText.toLowerCase();
+            if (title.includes(filter)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
+});
