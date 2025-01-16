6c68391b24f5d0ab8e9fd95ce3e39b3c5444d1fa
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-01-16 11:01:57 +0100

    Add open rate method to Email model to calculate how many emails are opened. Add this to template also.

--- emails/models.py
+++ emails/models.py
@@ -35,7 +35,18 @@ class Email(models.Model):
 
     def __str__(self):
         return self.subject
+    
+    def open_rate(self):
+        total_sent = self.email_list.count_emails()
+        opened_count = EmailTracking.objects.filter(email=self, opened_at__isnull=False).count()
 
+        if total_sent > 0:
+            open_rate = (opened_count / total_sent) * 100
+        else:
+            open_rate = 0
+
+        return round(open_rate, 2)
+    
 
 class Sent(models.Model):
     email = models.ForeignKey(Email, on_delete=models.CASCADE, blank=True, null=True)
