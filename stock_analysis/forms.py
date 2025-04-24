6f5097fd5730039be006f068a2266c5bac9d01dc
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-04-24 13:58:38 +0200

    Add small fix to django-autocomplete-light module. Now it is fixed.

--- stock_analysis/forms.py
+++ stock_analysis/forms.py
@@ -10,6 +10,9 @@ class StockForm(forms.Form):
     stock = forms.ModelChoiceField(
         queryset=Stock.objects.all(),
         widget=autocomplete.ModelSelect2(
-            url="stock_autocomplete"
+            url="stock_autocomplete",
+            attrs={
+                "data-placeholder": "Select a stock ..."
+            }
         )
     )
